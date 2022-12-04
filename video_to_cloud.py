7bc201c8c94f94d7d5e4c8983c92985637ffc71c
slavust <homer@slavust.name>
2022-12-04 13:57:44 +0000

    video_to_cloud.py works

--- video_to_cloud.py
+++ video_to_cloud.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 from moviepy.editor import VideoFileClip
 import tempfile
 import os.path
 import speech_to_cloud
+import argparse
 
 def convert_video_to_audio_moviepy(video_file, out_audio_path):
     """Converts video to audio using MoviePy library
@@ -10,12 +12,12 @@ def convert_video_to_audio_moviepy(video_file, out_audio_path):
     clip = VideoFileClip(video_file)
     clip.audio.write_audiofile(out_audio_path)
 
-def render_cloud_from_video(video_path, output_image_path, exclude_obvious):
+def render_cloud_from_video(video_path, output_image_path, language, exclude_obvious):
     with tempfile.TemporaryDirectory() as dir:
-        filename = os/path.split(video_path)[-1]
-        temp_audio_path = os.path.join(dir.path, filename, '.ogg')
+        filename = os.path.split(video_path)[-1]
+        temp_audio_path = os.path.join(dir, filename + '.wav')
         convert_video_to_audio_moviepy(video_path, temp_audio_path)
-        speech_to_cloud.render_cloud_from_audio(temp_audio_path, ooutput_image_path, exclude_obvious)
+        speech_to_cloud.render_cloud_from_audio(temp_audio_path, output_image_path, language, exclude_obvious)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -27,6 +29,7 @@ if __name__ == '__main__':
                         required=False, 
                         dest='remove_obvious',
                         action='store_true')
+    parser.add_argument('-l', '--lang', help='audio language', default='en', required=False)
     args = parser.parse_args()
     
-    render_cloud_from_video(args.video, args.output, args.remove_obvious)
+    render_cloud_from_video(args.video, args.output, args.lang, args.remove_obvious)
