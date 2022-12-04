85773b9181ec46c58c0d6660a698ed360f1be940
slavust <homer@slavust.name>
2022-12-04 13:37:53 +0000

    document_to_cloud.py works

--- document_to_cloud.py
+++ document_to_cloud.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
 import textract
 import text_to_cloud
+import argparse
 
 def render_wordcloud_from_document(doc_path, output_image_path, exclude_obvious):
-    text = textract.process(doc_path)
+    text = textract.process(doc_path, output_encoding='utf-8').decode('utf-8')
+    print(text)
     text_to_cloud.render_cloud_from_text(text, output_image_path, exclude_obvious)
 
 if __name__ == '__main__':
